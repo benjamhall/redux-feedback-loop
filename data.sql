492f1e708eab72b0faba44903dd31daa27c47423
Ben Hall <benjamhall@gmail.com>
2021-06-11 17:02:21 -0500

    set up database

--- data.sql
+++ data.sql
@@ -3,7 +3,7 @@ CREATE DATABASE "prime_feedback";
 
 -- Switch to "prime_feedback" before making:
 -- Table to store the feedback
-CREATE TABLE "feedback" (
+ CREATE TABLE "feedback" (
   "id" serial primary key,
   "feeling" INT not null,
   "understanding" INT not null,
@@ -11,7 +11,7 @@ CREATE TABLE "feedback" (
   "comments" text,
   "flagged" boolean default false,
   "date" date not null default CURRENT_DATE
-); 
+);
 
 -- Sample feedback item
 INSERT INTO "feedback" ("feeling", "understanding", "support", "comments")
