f104fa58fc6a1db720308646db004ef734838a7c
Ben Hall <benjamhall@gmail.com>
2021-06-12 13:55:24 -0500

    set up section in understanding and support

--- src/components/Support/Support.jsx
+++ src/components/Support/Support.jsx
@@ -15,7 +15,13 @@ function Support () {
     return (
         <div>
             <p>How well are you being supported?</p>
-            <input type="number" placeholder="number" id="number" required></input>
+            <select onChange={(event) => setInfo(event.target.value)}>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+            </select>
             <Button variant="outlined" color="primary" onClick={handleNext}>Next</Button>
         </div>
     );
